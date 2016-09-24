956bb4a08053d40d20e02c7063096fd9ce401da4
Karthik <karthik.g@techjini.com>
2016-09-24 18:39:52 +0530

    first commit

---
+++ home.js
@@ -0,0 +1,34 @@
+'use strict'
+
+angular.module('app.home',['ngRoute'])
+.config(['$routeProvider', function(rp) {
+  rp.when('/home',{
+    controller:"homectrl",
+    templateUrl:"hom.html"
+  })
+}])
+
+.controller('homectrl', ['$scope', '$http', function(scope, http){
+    http.get('employee.json')
+    .success(function(data) {
+    scope.emp=data.emp;
+  })
+    scope.name="home";
+}])
+
+// .filter('getMyKeys',function(){
+//   return function(e){
+//     var a=Object.keys(e);
+//     console.log(a);
+//   }
+// })
+.directive('d1', function(){
+  return {
+      restrict:'EA',
+      templateUrl:'home.html',
+      scope:{
+      emp :'='
+            },
+    transclude:true,
+    controller:'homectrl'
+  }});
